735acbf80b7a026239e56a52935da89f5e829644
vfow <165537678+vfow@users.noreply.github.com>
2026-08-30 23:29:14 +0800

    Add Vue Movie Details DOM owner

---
+++ frontend/src/media-details/MovieDetails.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { MovieDetailsViewModel } from './movieViewModel';
+
+defineProps<{
+  model: MovieDetailsViewModel;
+}>();
+</script>
+
+<template>
+  <div data-tvtracker-movie-details-owner="vue" v-html="model.html"></div>
+</template>
