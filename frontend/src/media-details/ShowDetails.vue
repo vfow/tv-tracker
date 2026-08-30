06758f867284f3592c382f8ffe61985e0490ce38
vfow <165537678+vfow@users.noreply.github.com>
2026-08-31 00:06:28 +0800

    Add Show Details Vue owner

---
+++ frontend/src/media-details/ShowDetails.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { ShowDetailsViewModel } from './showViewModel';
+
+defineProps<{
+  model: ShowDetailsViewModel;
+}>();
+</script>
+
+<template>
+  <div style="display: contents" data-tvtracker-show-details-owner="vue" v-html="model.html"></div>
+</template>
